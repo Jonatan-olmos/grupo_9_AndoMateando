459eb29128d0a33a28374cc2b9e454337b988c52
jonatan olmos <jon.ol.23.joo@gmail.con>
2023-11-14 06:20:14 -0300

    footer

--- app.js
+++ app.js
@@ -17,6 +17,16 @@ app.get('/login',(req,res) => res.sendFile(path.join(__dirname,'views','login.ht
 app.get('/productCart',(req,res) => res.sendFile(path.join(__dirname,'views','productCart.html')))
 /*Detalle del producto */
 app.get('/productDetail',(req,res) => res.sendFile(path.join(__dirname,'views','detail.html')))
+/*#mateartips*/
+app.get('/#mateartips',(req,res) => res.sendFile(path.join(__dirname,'views','#mateartips.html')))
+/*contacto */
+app.get('/contacto',(req,res) => res.sendFile(path.join(__dirname,'views','contacto.html')))
+/*mates_personalizados */
+app.get('/mates_personalizados',(req,res) => res.sendFile(path.join(__dirname,'views','mates_personalizados.html')))
+/*preguntas_frecuentes */
+app.get('/preguntas_frecuentes',(req,res) => res.sendFile(path.join(__dirname,'views','preguntas_frecuentes.html')))
+/*productos */
+app.get('/productos',(req,res) => res.sendFile(path.join(__dirname,'views','productos.html')))
 
 
 
