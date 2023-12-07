5a5968bed869c3bf1b5721786bfda24371167afa
facundo ramirez <ramirezfacundo1996@gmail.com>
2023-12-07 13:35:13 -0300

    modificandoDashboard

--- src/controllers/indexController.js
+++ src/controllers/indexController.js
@@ -1,32 +1,31 @@
-const { leerJSON } = require("../data")
-
+const { leerJSON } = require("../data");
 
 module.exports = {
-  index : (req,res) => {
-      const products = leerJSON('products');
-      
-      return res.render('index', {
-        products
-      })
+  index: (req, res) => {
+    const products = leerJSON("products");
+
+    return res.render("index", {
+      products,
+    });
   },
 
-  cart : (req,res) => {
-      return res.render('productCart')
+  cart: (req, res) => {
+    return res.render("productCart");
   },
- mateartips : (req,res) => {
-    return res.render('extras/mateartips')
-},
-preguntas_frecuentres : (req,res) => {
-  return res.render('extras/preguntas_frecuentes')
-},
-contacto : (req,res) => {
-  return res.render('extras/contacto')
-},
-  admin : (req,res) => {
-    const products = leerJSON('products');
+  mateartips: (req, res) => {
+    return res.render("extras/mateartips");
+  },
+  preguntas_frecuentres: (req, res) => {
+    return res.render("extras/preguntas_frecuentes");
+  },
+  contacto: (req, res) => {
+    return res.render("extras/contacto");
+  },
+  admin: (req, res) => {
+    const products = leerJSON("products");
 
-    return res.render('dashboard', {
-      products
-    })
-  }
-}
+    return res.render("dashboard", {
+      products,
+    });
+  },
+};
