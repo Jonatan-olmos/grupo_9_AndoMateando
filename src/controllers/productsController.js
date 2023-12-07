93dc1dbe8551f6fe740dac8ea9f40b8f5f909826
facundo ramirez <ramirezfacundo1996@gmail.com>
2023-12-07 16:46:20 -0300

    form1

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -1,23 +1,26 @@
 module.exports = {
-    detail : (req,res) => {
-        return res.render('products/detail')
-    },
-    mate : (req,res) => {
-        return res.render('products/mate')
-    },
-    mates_personalizados : (req,res) => {
-        return res.render('products/mates_personalizados')
-    },
-    set_yerbero : (req,res) => {
-        return res.render('products/set_yerbero')
-    },
-    super_combos : (req,res) => {
-        return res.render('products/super_combos')
-    },
-    termos : (req,res) => {
-        return res.render('products/termos')
-    },
-    add : (req,res) => {
-        return res.render('products/product-add')
-    }
-}
+  detail: (req, res) => {
+    return res.render("products/detail");
+  },
+  mate: (req, res) => {
+    return res.render("products/mate");
+  },
+  mates_personalizados: (req, res) => {
+    return res.render("products/mates_personalizados");
+  },
+  set_yerbero: (req, res) => {
+    return res.render("products/set_yerbero");
+  },
+  super_combos: (req, res) => {
+    return res.render("products/super_combos");
+  },
+  termos: (req, res) => {
+    return res.render("products/termos");
+  },
+  add: (req, res) => {
+    return res.render("products/product-add");
+  },
+  edit: (req, res) => {
+    return res.render("products/product-edit");
+  },
+};
--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -1,17 +1,26 @@
-const express = require('express');
-const { detail, mate, mates_personalizados, set_yerbero, super_combos, termos, add } = require('../controllers/productscontroller');
+const express = require("express");
+const {
+  detail,
+  mate,
+  mates_personalizados,
+  set_yerbero,
+  super_combos,
+  termos,
+  add,
+} = require("../controllers/productscontroller");
 
 const router = express.Router();
 
 /* /productos */
 
 router
-  .get('/detalle', detail)
-  .get('/mate', mate)
-  .get('/mates_personalizados', mates_personalizados)
-  .get('/set_yerbero', set_yerbero)
-  .get('/super_combos', super_combos)
-  .get('/termos', termos)
-  .get('/agregar', add)
+  .get("/detalle", detail)
+  .get("/mate", mate)
+  .get("/mates_personalizados", mates_personalizados)
+  .get("/set_yerbero", set_yerbero)
+  .get("/super_combos", super_combos)
+  .get("/termos", termos)
+  .get("/agregar", add)
+  .get("/editar/:id?", edit);
 
 module.exports = router;
