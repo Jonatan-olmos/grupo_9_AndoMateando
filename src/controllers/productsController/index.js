b4e332a80fdf8267c86c246f0fd6f79bab07619c
facundo ramirez <ramirezfacundo1996@gmail.com>
2023-12-31 18:17:25 -0300

    newVersion

---
+++ src/controllers/productsController/index.js
@@ -0,0 +1,11 @@
+const add = require("./add");
+const detail = require("./detail");
+const edit = require("./edit");
+const search = require("./search");
+
+module.exports = {
+  add,
+  detail,
+  edit,
+  search,
+};
