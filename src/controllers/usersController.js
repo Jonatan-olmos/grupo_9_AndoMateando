e89c6779e9ee4d0f3c451521225dba3d9ed13ae2
facundo ramirez <ramirezfacundo1996@gmail.com>
2024-01-04 04:22:38 -0300

    agregando estilos y funcionalidades

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,8 +1,28 @@
+const { leerJSON, escribirJSON } = require("../data/usuarios.json");
+
 module.exports = {
-    login : (req,res) => {
-        return res.render('users/login')
-    },
-    register : (req,res) => {
-        return res.render('users/register')
-    },
-}
+  login: (req, res) => {
+    return res.render("users/login");
+  },
+  register: (req, res) => {
+    return res.render("users/register");
+  },
+  createUser: (req, res) => {
+    const { nombre, email, telefono, contraseña } = req.body;
+
+    const newUser = {
+      nombre: nombre.trim(),
+      email: email.trim(),
+      telefono: telefono.trim(),
+      contraseña: contraseña.trim(),
+    };
+
+    const users = leerJSON("usuarios");
+
+    users.push(newUser);
+
+    escribirJSON(users, "usuarios");
+
+    return res.redirect("/carrito");
+  },
+};
--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,11 +1,12 @@
-const express = require('express');
-const { login, register } = require('../controllers/usersController');
+const express = require("express");
+const { login, register } = require("../controllers/usersController");
+const { createUser } = require("../controllers/usersController");
 
 const router = express.Router();
 
 /* /usuarios */
-router
-  .get('/ingreso',login)
-  .get('/registro',register)
+router.get("/ingreso", login).get("/registro", register);
+
+router.post("/registro", createUser);
 
 module.exports = router;
