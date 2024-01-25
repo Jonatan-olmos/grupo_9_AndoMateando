d5be6f7ef843a099d933e996297c40ca6257be2e
facundo ramirez <ramirezfacundo1996@gmail.com>
2024-01-25 02:45:37 -0300

    validaciones

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -7,21 +7,21 @@ const {
   contacto,
   admin,
   terminos_y_condiciones,
-
 } = require("../controllers/indexController");
-const checkAdmin = require('../middlewares/checkAdmin');
+const checkAdmin = require("../middlewares/checkAdmin");
 const router = express.Router();
 
 /* GET home page. */
 router
 
   .get("/", index)
- 
+
   .get("/carrito", cart)
   .get("/mateartips", mateartips)
   .get("/contacto", contacto)
   .get("/preguntas_frecuentes", preguntas_frecuentres)
-  .get("/admin", admin)
+  .get("/admin", checkAdmin, admin)
+  /* .get('/admin/productos/buscar' , checkAdmin, searchAdmin) "esta ruta esta comentada porque aun no se desarrollo"*/
   .get("/terminos-y-condiciones", terminos_y_condiciones);
 
 module.exports = router;
