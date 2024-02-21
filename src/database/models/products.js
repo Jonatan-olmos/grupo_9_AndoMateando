aced422d933cb1b09c3ed664edfb2c75eb47cb0f
facundo ramirez <ramirezfacundo1996@gmail.com>
2024-02-21 20:41:35 -0300

    inicio migracion

---
+++ src/database/models/products.js
@@ -0,0 +1,29 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Products extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Products.init({
+    name: DataTypes.STRING,
+    description: DataTypes.TEXT,
+    price: DataTypes.INTEGER,
+    discount: DataTypes.INTEGER,
+    quantityInStock: DataTypes.INTEGER,
+    categoryId: DataTypes.INTEGER,
+    brandId: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'Products',
+  });
+  return Products;
+};
